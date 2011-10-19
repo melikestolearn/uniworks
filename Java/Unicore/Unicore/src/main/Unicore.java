5bd9521d23922e50cf5629fb0060b47e308fcf9f
Elias <eparalikis@yahoo.de>
2011-10-19 17:49:26 +0200

    Unicore

---
+++ Java/Unicore/Unicore/src/main/Unicore.java
@@ -0,0 +1,18 @@
+package main;
+
+/** Program to connect computers. 
+ * This project is made for our time in university. 
+ * It combines many functions as chat, screen transfer, file transfer etc.
+ * 
+ */
+public class Unicore {
+
+	/** This is the main method.
+	 * @param args The method parameters.
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		Base base = new Base();
+	}
+
+}
