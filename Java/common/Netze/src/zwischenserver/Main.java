95d1044cb053645d73568a8dffb5a10c48c9c999
Elias <eparalikis@yahoo.de>
2011-10-23 20:57:13 +0200

    new files

--- Java/common/Netze/src/zwischenserver/Main.java
+++ Java/common/Netze/src/zwischenserver/Main.java
@@ -8,24 +8,26 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		new Zwischenserver("www.google.com", "Informatik", "Rechenmaschinenwissenschaft").run();
+		//new Zwischenserver("www.google.com", "Informatik", "Rechenmaschinenwissenschaft").run();
 		
-//		new Thread(new Runnable() {
-//			public void run() {
-//				try {
-//					new Zwischenserver("www.cs.hm.edu", "Informatik", "Rechenmaschinenwissenschaft").run();
-//					
-//				} catch (IOException e) {e.printStackTrace();}
-//			}
-//		});
-//		
-//		Thread.sleep(5000);
-//		
-//		
-//		String get = "GET /index.html HTTP/1.1\n\n";
-//		Socket s = new Socket("127.0.0.1", 8082);
-//		OutputStreamWriter w = new OutputStreamWriter(s.getOutputStream());
-//		w.write(get);
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					new Zwischenserver("www.cs.hm.edu", "Informatik", "Rechenmaschinenwissenschaft").run();
+					
+				} catch (IOException e) {e.printStackTrace();}
+			}
+		}).start();
+		
+		//Thread.sleep(5000);
+		
+		
+		String get = "GET / HTTP/1.1\n\n";
+		//GET / HTTP/1.1\n\n";
+		Socket s = new Socket("127.0.0.1", 8082);
+		OutputStreamWriter w = new OutputStreamWriter(s.getOutputStream());
+		w.write(get);
+		w.flush();
 	}
 
 }
